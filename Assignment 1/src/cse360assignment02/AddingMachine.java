543cb7ec3633069688f1bb5530a7e84093ddca07
andyl <andyl@DESKTOP-AIEKVQD>
2020-10-05 13:14:02 -0700

    Added a history String Completed getTotal() Completed add(value) Completed subtract(value) Completed toString() Completed clear() No bug testing has occurred, if there are no more commits, there was 	nothing to fix.

--- Assignment 1/src/cse360assignment02/AddingMachine.java
+++ Assignment 1/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,37 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history;
   
+  //Initialize the total and history string
   public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+    total = 0;  //initialize the total to 0
+    history = "0";  //initialize the history string to ' 0 '
   }
   
+  //returns the total
   public int getTotal () {
-    return 0;
+    return total;   //return total
   }
   
+  //adds value to total and updates history accordingly
   public void add (int value) {
+	  total = total + value;   //adds value to total
+	  history = history + " + " + value;   //updates history
   }
 
+  //subtracts value from total and updates history accordingly
   public void subtract (int value) {
+	  total = total - value;   //subtracts value from total
+	  history = history + " - " + value;   //updates history
   }
 
+  //returns the history string
   public String toString () {
-    return "";
+    return history;   //returns history
   }
 
   public void clear() {
+	  System.gc();
   }
 }
